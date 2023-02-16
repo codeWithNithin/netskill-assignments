ec3b6cc552c260cfbb1181cd676e5029dd053b0a
nithin kumar <nithinkottary25@gmail.com>
2023-02-16 20:18:15 +0530

    completed 2nd assignment

---
+++ unit-1/chapter-2/assignment2.js
@@ -0,0 +1,10 @@
+// ASSIGNMENT - Create a variable called age and assign it a integer. if age is more than 18, then print 'yes' else print 'no'
+const age = 25
+
+// Method 1
+// if (age > 18) console.log('yes')
+// else console.log('no')
+
+// Method 2
+const result = age > 18 ? 'yes' : 'no'
+console.log(result)
