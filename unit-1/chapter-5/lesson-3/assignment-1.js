ba37ef951592bd631ea6455cbb643829e660cde1
nithin kumar <nithinkottary25@gmail.com>
2023-03-13 17:35:10 +0530

    completed chapter-5/lessoon-3 assignment

--- unit-1/chapter-5/lesson-3/assignment-1.js
+++ unit-1/chapter-5/lesson-3/assignment-1.js
@@ -1,4 +1,7 @@
 // write a for loop and use label "loop1" which iterates through number 1 to 100
 // and if number is less than 200 then it continues to "loop1" else it prints the number
 
-for (let loop1 = 1; loop1 <= 100; loop1++) {}
+for (let i = 1; i <= 100; i++) {
+  if (i < 200) console.log('loop1')
+  else console.log(`number ${i}`)
+}
